9c88b320b9b9ee1b8120bc0a4874fc80067ef57f
Benson Chen <tumlivein@gmail.com>
2025-08-03 00:43:04 +0800

    feat: 8. Connections & Pagination - Infinite Scrolling Newsfeed Stories

--- newsfeed/src/components/Newsfeed.tsx
+++ newsfeed/src/components/Newsfeed.tsx
@@ -1,29 +1,69 @@
 import * as React from "react";
 import { graphql } from 'relay-runtime';
-import { useLazyLoadQuery } from 'react-relay';
+import { useFragment, useLazyLoadQuery, usePaginationFragment } from 'react-relay';
 import { NewsfeedQuery as NewsfeedQueryType } from "./__generated__/NewsfeedQuery.graphql";
+import type { NewsfeedContentsRefetchQuery as NewsfeedContentsRefetchQueryType } from "./__generated__/NewsfeedContentsRefetchQuery.graphql";
+import { NewsfeedContentsFragment$key } from "./__generated__/NewsfeedContentsFragment.graphql";
 
 import Story from "./Story";
+import InfiniteScrollTrigger from "./InfiniteScrollTrigger";
 
 const NewsfeedQuery = graphql`
   query NewsfeedQuery {
-    topStories {
-      id
-      ...StoryFragment
+    ...NewsfeedContentsFragment
+  }
+`;
+
+const NewsfeedContentsFragment = graphql`
+  fragment NewsfeedContentsFragment on Query
+   @refetchable(queryName: "NewsfeedContentsRefetchQuery")
+   @argumentDefinitions(
+    cursor: { type: "String" },
+    count: { type: "Int", defaultValue: 3 }
+   )
+  {
+    viewer {
+      newsfeedStories (first: $count, after: $cursor)
+        @connection(key: "NewsfeedContentsFragment_newsfeedStories")
+      {
+        edges {
+          node {
+            id
+            ...StoryFragment
+          }
+        }
+      }
     }
   }
 `;
 
 export default function Newsfeed() {
-  const data = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
+  const queryData = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
+  const {
+    data,
+    loadNext,
+    hasNext,
+    isLoadingNext,
+  } = usePaginationFragment<
+    NewsfeedContentsRefetchQueryType,
+    NewsfeedContentsFragment$key
+  >(NewsfeedContentsFragment, queryData);
+  const storyEdges = data.viewer.newsfeedStories.edges;
 
-  const stories = data.topStories;
+  function onEndReached() {
+    loadNext(1);
+  }
 
   return (
     <div className="newsfeed">
-      {stories.map((story) => (
-        <Story key={story.id} story={story} />
+      {storyEdges.map((storyEdge) => (
+        <Story key={storyEdge.node.id} story={storyEdge.node} />
       ))}
+      <InfiniteScrollTrigger
+        hasNext={hasNext}
+        isLoadingNext={isLoadingNext}
+        onEndReached={onEndReached}
+      />
     </div>
   );
 }
